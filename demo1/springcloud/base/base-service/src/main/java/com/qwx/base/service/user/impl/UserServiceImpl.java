f1fb934c505b0f2f7c40e5a1a3cc5ba9ad9ccfc8
qinwenxu <786002003@qq.com>
2019-12-18 07:25:54 +0800

    first commit

--- demo1/springcloud/base/base-service/src/main/java/com/qwx/base/service/user/impl/UserServiceImpl.java
+++ demo1/springcloud/base/base-service/src/main/java/com/qwx/base/service/user/impl/UserServiceImpl.java
@@ -3,9 +3,11 @@ package com.qwx.base.service.user.impl;
 import com.qwx.base.dao.user.UserMapper;
 import com.qwx.base.service.user.IUserService;
 import com.qwx.common.base.entity.user.User;
+import com.qwx.common.utils.RedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 @Service
 public class UserServiceImpl implements IUserService {
@@ -13,9 +15,14 @@ public class UserServiceImpl implements IUserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Resource
+    private RedisUtils redisUtils;
+
+
     @Override
     public List<User> getUserList() {
         List<User> userList = userMapper.getUserList();
+        //redisUtils.set("p123", "666");
         return userList;
     }
 }
